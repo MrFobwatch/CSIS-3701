94b6459a348d3be9e706d366ef4925d01d95e863
Unknown <kcy0511@gmail.com>
2018-09-10 20:50:22 -0400

    Implement Default Method and Instance Example

--- IntelliJ/Classwork/Inheritance/src/simpletimeclient/TestSimpleTimeClient.java
+++ IntelliJ/Classwork/Inheritance/src/simpletimeclient/TestSimpleTimeClient.java
@@ -1,4 +1,9 @@
 package simpletimeclient;
 
 public class TestSimpleTimeClient {
+	public static void main(String[] args) {
+		TimeClient myTimeClient = new SimpleTimeClient();
+		System.out.println("Current time: " + myTimeClient.toString());
+		System.out.println("Time in California: " + myTimeClient.getZonedDateTime("Blah blah").toString());
+	}
 }
--- IntelliJ/Classwork/Inheritance/src/simpletimeclient/TimeClient.java
+++ IntelliJ/Classwork/Inheritance/src/simpletimeclient/TimeClient.java
@@ -1,4 +1,24 @@
 package simpletimeclient;
 
-public class TimeClient {
+import java.time.*;
+import java.util.*;
+
+public interface TimeClient {
+	void setTime(int hour, int month, int year);
+	void setDate(int day, int month, int year);
+	void setDateAndTime(int day, int month, int year, int hour, int minute, int second);
+	LocalDateTime getLocalDateTime();
+
+	static ZoneId getZoneId (String zoneString){
+		try {
+			return ZoneId.of(zoneString);
+		}
+		catch (DateTimeException e){
+			System.err.println("Invalid time zone: " + zoneString + "; using default time zone instead.");
+			return ZoneId.systemDefault();
+		}
+	}
+	default ZonedDateTime getZonedDateTime(String zoneString) {
+		return ZonedDateTime.of(getLocalDateTime(), getZoneId(zoneString));
+	}
 }
