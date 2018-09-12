361ebfa0b48a08ec9a20b5e79f8127878ae95b26
MrFobwatch <kcy0511@gmail.com>
2018-09-12 19:18:03 -0400

    Create Sphere class

--- IntelliJ/Classwork/Inheritance/src/circledemo/CircleDemo.java
+++ IntelliJ/Classwork/Inheritance/src/circledemo/CircleDemo.java
@@ -3,7 +3,7 @@ package circledemo;
 public class CircleDemo {
 	public static void main(String[] args) {
 		Cylinder myCylinder = new Cylinder(4, 4);
-		System.out.println(myCylinder.getHeight());
-		System.out.println(myCylinder.getVolume());
+		System.out.println("The height is: " + myCylinder.getHeight());
+		System.out.println("The volume is: " + myCylinder.getVolume());
 	}
 }
---
+++ IntelliJ/Classwork/Inheritance/src/circledemo/Sphere.java
@@ -0,0 +1,13 @@
+package circledemo;
+
+public class Sphere extends Circle {
+	/**
+	 * The Sphere class with radius r
+	 *
+	 * @param r The radius
+	 */
+
+	public Sphere(int r) {
+		super(r);
+	}
+}
