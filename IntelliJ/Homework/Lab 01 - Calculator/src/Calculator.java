e468a353852c5a12484ac9cca65ebe7a36d09587
Unknown <kcy0511@gmail.com>
2018-09-22 01:10:26 -0400

    Cleanup Code and Add comments

--- IntelliJ/Homework/Lab 01 - Calculator/src/Calculator.java
+++ IntelliJ/Homework/Lab 01 - Calculator/src/Calculator.java
@@ -15,7 +15,7 @@ public class Calculator {
 	private double second;
 	private String operator;
 
-	public double getTotal() {
+	private double getTotal() {
 		return total;
 	}
 
@@ -95,29 +95,25 @@ public class Calculator {
 		Boolean firstValue = FALSE;
 		Boolean secondValue = FALSE; //Do we have a first and second number
 		System.out.println("Enter a single operation Expression ");
-		while (!scan.hasNext("[-+*/]")) {
-			if (!firstValue && scan.hasNextDouble()) {
+		while (!scan.hasNext("[-+*/]")) { //Check that input doesn't start with an operator
+			if (!firstValue && scan.hasNextDouble()) { //Check if we don't have a first number and that the next input is a number
 				first = scan.nextDouble();
 				firstValue = TRUE;
-				if (scan.hasNext("[-+*/]")) {
+				if (scan.hasNext("[-+*/]")) { //If the input has one of the operators next add it
 					operator = scan.next();
 				}
-			} else if (!secondValue && scan.hasNextDouble()) {
+			} else if (!secondValue && scan.hasNextDouble()) { //Check that we don't have a second number and that the next input is number
 				second = scan.nextDouble();
 				secondValue = TRUE;
 			}
-
-			if ((!firstValue && !secondValue) || operator == null) {
+			if ((!firstValue && !secondValue) || operator == null) { //Checks that we have both numbers and the operator
 				System.out.println("Please Check your Input for numbers and correct symbols and try again");
 				firstValue = FALSE;
 				secondValue = FALSE;
-
 				scan.nextLine();
 			}
-			if (firstValue && secondValue) break;
+			if (firstValue && secondValue) break; //We have both numbers so exit loop
 		}
-
-
 	}
 
 	public void run() {
