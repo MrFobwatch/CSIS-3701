7401768f3cf0c237fa6b83030140353530ce3b09
MrFobwatch <kcy0511@gmail.com>
2018-10-28 00:07:04 -0400

    Finish up some things before I leave

--- IntelliJ/Classwork/GuessApp/src/main/java/guessgame/GuessApp.java
+++ IntelliJ/Classwork/GuessApp/src/main/java/guessgame/GuessApp.java
@@ -1,25 +1,43 @@
 package guessgame;
 
 public class GuessApp {
-
+	private static final int defaultUpperBound = 10;
+	private static final int defaultLowerBound = 1;
+	public int lowerBound = defaultLowerBound;
+	public int upperBound = defaultUpperBound;
+	private GuessLogic game;
 
 	public GuessApp() {
-		GuessLogic game = new GuessLogic();
+		GuessLogic game = new GuessLogic(defaultLowerBound, defaultUpperBound);
+	}
+
+	public GuessApp(int upperBound) {
+		GuessLogic game = new GuessLogic(defaultLowerBound, upperBound);
+		this.upperBound = upperBound;
+	}
+
+	public GuessApp(int lowerBound, int upperBound) {
+		GuessLogic game = new GuessLogic(lowerBound, upperBound);
+		this.lowerBound = lowerBound;
+		this.upperBound = upperBound;
+	}
+
+	public void gamePlay() {
 	}
 
 	private void turn() {
-
 	}
 
-	private void startGame() {
-
+	private void printPrompt() {
+		System.out.print("Enter a guess between ");
+		System.out.print(lowerBound);
+		System.out.print(" and ");
+		System.out.print(upperBound + ": " )
 	}
 
 	private int getUserGuess() {
-		int num= -1 ;
+		int num = -1;
 
 		return num;
 	}
 }
-
-
