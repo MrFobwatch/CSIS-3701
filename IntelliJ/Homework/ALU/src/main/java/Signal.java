129a779301b819fcd2cd2f14f4a09891ddd864b5
Unknown <kcy0511@gmail.com>
2018-11-13 00:22:25 -0500

    Add Maven Support

---
+++ IntelliJ/Homework/ALU/src/main/java/Signal.java
@@ -0,0 +1,44 @@
+public class Signal {
+    private boolean state;
+    private boolean highZ;
+
+    public Signal(boolean state) {
+        this.state = state;
+        highZ = false;
+    }
+
+    public Signal() {
+        state = false;
+        highZ = false;
+    }
+
+    public void setHighZ(boolean highZ) {
+        this.highZ = highZ;
+    }
+
+    private boolean isState() {
+        return state;
+    }
+
+    public boolean isHighZ() {
+        return highZ;
+    }
+
+    public int getValue() {
+        int value = -1;
+        if (state) {
+            value = 1;
+        } else if (!state) {
+            value = 0;
+        }
+        return value;
+    }
+
+    public void receive(int incomingSignal) {
+        if (incomingSignal == 1) {
+            state = true;
+        } else if (incomingSignal == 0) {
+            state = false;
+        }
+    }
+}
