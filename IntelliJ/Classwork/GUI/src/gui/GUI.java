93b171d04bc307e06dc31791e8c3d0efb6adcabc
Unknown <kcy0511@gmail.com>
2018-11-16 15:36:21 -0500

    Fix guiChat

---
+++ IntelliJ/Classwork/GUI/src/gui/GUI.java
@@ -0,0 +1,11 @@
+package gui;
+
+import javax.swing.*;
+
+public class GUI {
+    public static void main(String[] args) {
+        //
+        JFrame frame = new JFrame("My First GUI");
+
+    }
+}
