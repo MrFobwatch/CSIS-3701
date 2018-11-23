b98f787d5d36dd7ca5968047d36049cd62f6b3b9
MrFobwatch <kcy0511@gmail.com>
2018-11-23 18:03:35 -0500

    Create skeleton NorGate

---
+++ IntelliJ/Homework/ALU/src/main/java/NorGate.java
@@ -0,0 +1,3 @@
+public class NorGate extends OrGate {
+
+}
---
+++ IntelliJ/Homework/ALU/src/test/java/NorGateTest.java
@@ -0,0 +1,4 @@
+public class NorGateTest {
+}
+
+//Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
