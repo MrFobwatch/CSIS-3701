3261beeff5464300fae97de8a376c1bf4cf6ec53
MrFobwatch <kcy0511@gmail.com>
2018-11-26 23:14:57 -0500

    Add Some tests for HalfAdder

--- IntelliJ/Homework/ALU/src/test/java/HalfAdderTest.java
+++ IntelliJ/Homework/ALU/src/test/java/HalfAdderTest.java
@@ -1,66 +1,94 @@
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 
 import java.util.LinkedList;
-import java.util.ListIterator;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.anyInt;
-import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class HalfAdderTest {
     @Spy LinkedList<Signal> outputList;
-    @Mock Signal A;
-    @Mock Signal B;
-    @Mock Signal Sum;
-    @Mock Signal Cout;
-    @Mock XorGate XOR;
-    @Mock AndGate AND;
+    @Spy Signal A;
+    @Spy Signal B;
+    @Spy Signal Sum;
+    @Spy Signal Cout;
+    @Spy XorGate XOR;
+    @Spy AndGate AND;
     @InjectMocks HalfAdder halfAdder;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        when(A.isState(), thenReturn(true));
     }
 
     @Test
     public void testReceiveInput() throws Exception {
         halfAdder.receiveInput(new Signal(true), new Signal(true));
-        assertThat(A.isState(),is(equalTo(true)));
-        assertThat(B.isState(), is(equalTo(true)));
+        assertThat(halfAdder.A.isState(), is(equalTo(true)));
+        assertThat(halfAdder.B.isState(), is(equalTo(true)));
     }
 
     @Test
-    public void testReturnOutputs() throws Exception {
-//        ListIterator result = halfAdder.returnOutputs();
-	    int index = 0;
-	    for (ListIterator outputs = halfAdder.returnOutputs(); outputs.hasNext(); ) {
-		    Signal result = (Signal) outputs.next();
-		    Assert.assertThat(result, is(equalTo(true)));
-	    }
-    }
-
-
-    @Test
-    public void testDoOperation() throws Exception {
-        when(XOR.getOutputAtPort(anyInt())).thenReturn(new Signal(true));
-        when(AND.getOutputAtPort(anyInt())).thenReturn(new Signal(true));
-//        when(A.getValue().thenReturn(1));
-
+    public void testDoOperationReturnSum00() throws Exception {
+        Signal Value0 = mock(Signal.class);
+        Signal Value1 = mock(Signal.class);
+        when(Value0.isState()).thenReturn(false);
+        when(Value1.isState()).thenReturn(false);
+        halfAdder.receiveInput(Value0, Value1);
         halfAdder.doOperation();
-        assertThat(Sum.isState(), is(equalTo(true)));
-        assertThat(Cout.isState(), is(equalTo(true)));
+        assertThat(halfAdder.getSum().getValue(), is(equalTo(0)));
+        assertThat(halfAdder.getCout().getValue(), is(equalTo(1)));
     }
+
+    @Test
+    public void testDoOperationReturnSum11() throws Exception {
+        Signal Value0 = mock(Signal.class);
+        Signal Value1 = mock(Signal.class);
+        when(Value0.isState()).thenReturn(true);
+        when(Value1.isState()).thenReturn(true);
+        halfAdder.receiveInput(Value0, Value1);
+        halfAdder.doOperation();
+        assertThat(halfAdder.getSum().getValue(), is(equalTo(0)));
+        assertThat(halfAdder.getCout().getValue(), is(equalTo(1)));
+    }
+
+    @Test
+    public void testDoOperationReturnSum10() throws Exception {
+        Signal Value0 = mock(Signal.class);
+        Signal Value1 = mock(Signal.class);
+        when(Value0.isState()).thenReturn(true);
+        when(Value1.isState()).thenReturn(false);
+        halfAdder.receiveInput(Value0, Value1);
+        halfAdder.doOperation();
+        assertThat(halfAdder.getSum().getValue(), is(equalTo(1)));
+        assertThat(halfAdder.getCout().getValue(), is(equalTo(0)));
+    }
+
+    @Test
+    public void testDoOperationReturnSum01() throws Exception {
+        Signal Value0 = mock(Signal.class);
+        Signal Value1 = mock(Signal.class);
+        when(Value0.isState()).thenReturn(false);
+        when(Value1.isState()).thenReturn(true);
+        halfAdder.receiveInput(Value0, Value1);
+        halfAdder.doOperation();
+        assertThat(halfAdder.getSum().getValue(), is(equalTo(1)));
+        assertThat(halfAdder.getCout().getValue(), is(equalTo(0)));
+    }
+
+//    @Test
+//    public void testDoOperationReturnCout() throws Exception {
+//        when(XOR.getOutputAtPort(anyInt())).thenReturn(A);
+//        when(AND.getOutputAtPort(anyInt())).thenReturn(B);
+//        halfAdder.doOperation();
+//        assertThat(halfAdder.getCout().getValue(), is(equalTo(1)));
+//    }
 }
 
 // Generated with love by TestMe :) Please report issues and submit feature requests at:
