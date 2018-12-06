6fff4b8ce7119839e7dc59d0deaf66347145aaf1
MrFobwatch <kcy0511@gmail.com>
2018-12-05 19:39:22 -0500

    Presentation

---
+++ IntelliJ/Homework/ALU/src/main/java/MultiBitSignal.java
@@ -0,0 +1,57 @@
+import java.util.Iterator;
+import java.util.Vector;
+
+public class MultiBitSignal {
+    private Vector<Signal> bitVector = new Vector<>();
+    private int decimal;
+
+
+    private void setDecimal() {
+    	String binaryValue = "";
+    	Iterator<Signal> binaryIterator = bitVector.iterator();
+    	while (binaryIterator.hasNext()){
+    		int bitValue = binaryIterator.next().getValue();
+    		binaryValue += binaryValue + Integer.toString(bitValue);
+	    }
+        decimal = Integer.parseInt(binaryValue, 2);
+    }
+
+    public MultiBitSignal(Signal... numOfBit) {
+        bitVector.setSize(numOfBit.length);
+        for (int bitCount = 0; bitCount < numOfBit.length; bitCount++) {
+            Signal singleBit = numOfBit[bitCount];
+            bitVector.set(bitCount, singleBit);
+        }
+        setDecimal();
+    }
+
+    public void receiveInput(Signal... numOfBit) {
+        bitVector.setSize(numOfBit.length);
+        for (int bitCount = 0; bitCount < numOfBit.length; bitCount++) {
+            Signal singleBit = numOfBit[bitCount];
+            bitVector.set(bitCount, singleBit);
+        }
+        setDecimal();
+    }
+
+    public void receiveInput(String numberInBinary) {
+        bitVector.setSize(numberInBinary.length());
+        for (int bitCount = 0; bitCount < numberInBinary.length(); bitCount++) {
+            char bitValue = numberInBinary.charAt(bitCount);
+            if (bitValue == '1') {
+                bitVector.elementAt(bitCount).changeState(true);
+            } else if (bitValue == '0') {
+                bitVector.elementAt(bitCount).changeState(true);
+            }
+        }
+        setDecimal();
+    }
+
+    public int getDecimal() {
+        return decimal;
+    }
+
+    public Vector<Signal> getBitVector() {
+        return bitVector;
+    }
+}
