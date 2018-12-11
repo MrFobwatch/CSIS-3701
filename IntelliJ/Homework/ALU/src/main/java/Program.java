b31fa3ed1f6d3a3873cb1614f24d9376fdd37f91
MrFobwatch <kcy0511@gmail.com>
2018-12-10 23:28:38 -0500

    Write code for main

--- IntelliJ/Homework/ALU/src/main/java/Program.java
+++ IntelliJ/Homework/ALU/src/main/java/Program.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Program {
     public static void main(String[] args) {
-        //
-    }
+        MultiBitAdderSubtractor FourBitAdderSubtractor = new MultiBitAdderSubtractor(4);
+        int firstNum = 11;
+        int secondNum= 10;
+        MultiBitSignal firstNumSignal = new MultiBitSignal(Integer.toBinaryString(firstNum)); //Convert Decimal Numbers to Binary
+        MultiBitSignal secondNumSignal = new MultiBitSignal(Integer.toBinaryString(secondNum));
+        List<Signal> fullInputList = new ArrayList<>();
+        fullInputList.addAll(firstNumSignal.getBitList());
+        fullInputList.addAll(secondNumSignal.getBitList());
+        FourBitAdderSubtractor.receiveInputs(fullInputList);
+        FourBitAdderSubtractor.enableAddition();
+        FourBitAdderSubtractor.doOperation();
+        FourBitAdderSubtractor.returnOutputs();
+            }
 }
